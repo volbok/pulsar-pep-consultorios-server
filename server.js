510a2c2997662492710420f5f94cb27b7f00660a
volbok <rodrigocarvalholessa@gmail.com>
2024-11-10 19:44:13 -0300

    MELHORIAS GERAIS E IMPLEMENTAÇÃO DA GUIA TISS-SADT

--- server.js
+++ server.js
@@ -356,9 +356,10 @@ app.post("/insert_paciente", (req, res) => {
     convenio_carteira,
     validade_carteira,
     nome_social,
+    obs,
   } = req.body;
   var sql =
-    "INSERT INTO paciente (nome_paciente, nome_mae_paciente, dn_paciente, antecedentes_pessoais, medicacoes_previas, exames_previos, exames_atuais, tipo_documento, numero_documento, cns, endereco, logradouro, bairro, localidade, uf, cep, telefone, email, nome_responsavel, sexo, nacionalidade, cor, etnia, orgao_emissor, endereco_numero, endereco_complemento, convenio_nome, convenio_codigo, convenio_carteira, validade_carteira, nome_social) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31)";
+    "INSERT INTO paciente (nome_paciente, nome_mae_paciente, dn_paciente, antecedentes_pessoais, medicacoes_previas, exames_previos, exames_atuais, tipo_documento, numero_documento, cns, endereco, logradouro, bairro, localidade, uf, cep, telefone, email, nome_responsavel, sexo, nacionalidade, cor, etnia, orgao_emissor, endereco_numero, endereco_complemento, convenio_nome, convenio_codigo, convenio_carteira, validade_carteira, nome_social, obs) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32)";
   pool.query(
     sql,
     [
@@ -393,6 +394,7 @@ app.post("/insert_paciente", (req, res) => {
       convenio_carteira,
       validade_carteira,
       nome_social,
+      obs,
     ],
     (error, results) => {
       if (error)
@@ -437,9 +439,10 @@ app.post("/update_paciente/:id_paciente", (req, res) => {
     convenio_carteira,
     validade_carteira,
     nome_social,
+    obs,
   } = req.body;
   var sql =
-    "UPDATE paciente SET nome_paciente = $1, nome_mae_paciente = $2, dn_paciente = $3, antecedentes_pessoais = $4, medicacoes_previas = $5, exames_previos = $6, exames_atuais = $7, tipo_documento = $8, numero_documento = $9, cns = $10, endereco = $11, logradouro = $12, bairro = $13, localidade = $14, uf = $15, cep = $16, telefone = $17, email = $18, nome_responsavel = $19, sexo = $20, nacionalidade = $21, cor = $22, etnia = $23, orgao_emissor = $24, endereco_numero = $25, endereco_complemento = $26, convenio_nome = $27, convenio_codigo = $28, convenio_carteira = $29, validade_carteira = $30, nome_social = $31 WHERE id_paciente = $32";
+    "UPDATE paciente SET nome_paciente = $1, nome_mae_paciente = $2, dn_paciente = $3, antecedentes_pessoais = $4, medicacoes_previas = $5, exames_previos = $6, exames_atuais = $7, tipo_documento = $8, numero_documento = $9, cns = $10, endereco = $11, logradouro = $12, bairro = $13, localidade = $14, uf = $15, cep = $16, telefone = $17, email = $18, nome_responsavel = $19, sexo = $20, nacionalidade = $21, cor = $22, etnia = $23, orgao_emissor = $24, endereco_numero = $25, endereco_complemento = $26, convenio_nome = $27, convenio_codigo = $28, convenio_carteira = $29, validade_carteira = $30, nome_social = $31, obs = $32 WHERE id_paciente = $33";
   pool.query(
     sql,
     [
@@ -474,6 +477,7 @@ app.post("/update_paciente/:id_paciente", (req, res) => {
       convenio_carteira,
       validade_carteira,
       nome_social,
+      obs,
       id_paciente,
     ],
     (error, results) => {
